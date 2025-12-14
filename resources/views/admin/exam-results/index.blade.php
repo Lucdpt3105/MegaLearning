934e9fb362455ee6cc1e77c5886fe6a5266a87d6
minh7709 <minh2k32003@gmail.com>
2025-12-14 21:42:59 +0700

    done

--- resources/views/admin/exam-results/index.blade.php
+++ resources/views/admin/exam-results/index.blade.php
@@ -196,14 +196,4 @@
 </div>
 
 @endsection
-                </tr>
-                @empty
-                <tr>
-                    <td colspan="6" class="px-6 py-4 text-center text-gray-500">Chưa có dữ liệu</td>
-                </tr>
-                @endforelse
-            </tbody>
-        </table>
-    </div>
-</div>
-@endsection
+
